e2088802a4caa63db266c2238952d2842e3b2758
Aditya Kumar <151712430+adityakumar143@users.noreply.github.com>
2025-05-27 23:56:01 +0530

    Add files via upload

---
+++ src/main/java/com/exmple/App.java
@@ -0,0 +1,14 @@
+package com.exmple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=10,b=10;
+        System.out.println(a+b);
+    }
+}
